b680917460aa5a0fe8c6e452a9493dcba4ba2d24
zachsg <zach@bindl.co>
2023-09-06 15:44:21 -0700

    upgrade packages, bump version

--- lib/features/auth/auth.g.dart
+++ lib/features/auth/auth.g.dart
@@ -21,4 +21,4 @@ final authProvider = AutoDisposeNotifierProvider<Auth, AuthModel>.internal(
 
 typedef _$Auth = AutoDisposeNotifier<AuthModel>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/features/habitat/habitat.g.dart
+++ lib/features/habitat/habitat.g.dart
@@ -168,4 +168,4 @@ class _HabitatProviderElement
   HUHabitatModel get habitat => (origin as HabitatProvider).habitat;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
