dc2f492fd08a9790bb993ab39f291f9878f6b05b
zachsg <zach@bindl.co>
2023-10-12 18:28:17 -0700

    upgrade libs

--- lib/features/grow/grow.g.dart
+++ lib/features/grow/grow.g.dart
@@ -55,6 +55,7 @@ class GrowFamily extends Family<GrowModel> {
     );
   }
 
+  @visibleForOverriding
   @override
   GrowProvider getProviderOverride(
     covariant GrowProvider provider,
@@ -165,4 +166,4 @@ class _GrowProviderElement extends NotifierProviderElement<Grow, GrowModel>
       (origin as GrowProvider).habitatAndAction;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter
