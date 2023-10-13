15d316c8aa4288f0a68f3f84a8514495e94def8e
zachsg <zach@bindl.co>
2023-10-12 18:28:17 -0700

    upgrade libs

--- lib/features/habitat_settings/habitat_settings.g.dart
+++ lib/features/habitat_settings/habitat_settings.g.dart
@@ -56,6 +56,7 @@ class HabitatSettingsFamily extends Family<HabitatSettingsModel> {
     );
   }
 
+  @visibleForOverriding
   @override
   HabitatSettingsProvider getProviderOverride(
     covariant HabitatSettingsProvider provider,
@@ -172,4 +173,4 @@ class _HabitatSettingsProviderElement
   HUHabitatModel get habitat => (origin as HabitatSettingsProvider).habitat;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter
