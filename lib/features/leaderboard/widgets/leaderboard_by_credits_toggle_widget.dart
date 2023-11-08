5b25dfcfb369595111bc0237b3dbde254fec52eb
zachsg <zach@bindl.co>
2023-11-08 16:15:11 -0700

    remove switch widget and replace with iconbutton on leaderboard

--- lib/features/leaderboard/widgets/leaderboard_by_credits_toggle_widget.dart
+++ lib/features/leaderboard/widgets/leaderboard_by_credits_toggle_widget.dart
@@ -3,58 +3,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../leaderboard.dart';
 
-class LeaderboardByCreditsToggleWidget extends ConsumerStatefulWidget {
+class LeaderboardByCreditsToggleWidget extends ConsumerWidget {
   const LeaderboardByCreditsToggleWidget({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _LeaderboardByCreditsToggleWidgetState();
-}
-
-class _LeaderboardByCreditsToggleWidgetState
-    extends ConsumerState<LeaderboardByCreditsToggleWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      duration: const Duration(seconds: 10),
-      vsync: this,
-    )..repeat();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final provider = ref.watch(leaderboardProvider);
-    return Row(
-      children: [
-        GestureDetector(
-            onTap: () => ref
-                .read(leaderboardProvider.notifier)
-                .setIsByCredit(!provider.byCredit),
-            child: const Icon(Icons.schedule)),
-        AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Switch.adaptive(
-              activeColor: Theme.of(context).colorScheme.primary,
-              value: provider.byCredit,
-              onChanged: (isOn) {
-                ref.read(leaderboardProvider.notifier).setIsByCredit(isOn);
-              },
-            );
-          },
-        ),
-        const SizedBox(width: 4.0),
-      ],
+
+    return IconButton(
+      onPressed: () => ref
+          .read(leaderboardProvider.notifier)
+          .setIsByCredit(!provider.byCredit),
+      icon:
+          Icon(provider.byCredit ? Icons.schedule : Icons.scoreboard_outlined),
     );
   }
 }
